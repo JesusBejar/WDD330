97ebb427a94f879307033eb98efa1394039e7770
Jesus Bejar <israelbejarii@gmail.com>
2023-05-01 13:48:43 -0600

    developed date.js file

---
+++ HOMEPORTAL/scripts/date.js
@@ -0,0 +1,7 @@
+// Getting the current year
+const date1 = document.querySelector("#date1");
+date1.innerHTML = new Date().getFullYear();
+
+
+const date2 = document.querySelector("#date2");
+date2.innerHTML = new Date(document.lastModified).toLocaleDateString("en-US");
