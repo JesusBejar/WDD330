cf11b75e2b0cc979c2759a98a63d1ecaa6eeb8fe
Jesus Bejar <israelbejarii@gmail.com>
2023-05-03 09:13:59 -0600

    made 3 simple functions

---
+++ ArrowFunc/practice.js
@@ -0,0 +1,29 @@
+// 3 different ways to write a function - 
+// simple function
+function sum (a, b)
+{
+    return a + b
+}
+// simple arrow function
+let sum2 = (a, b) =>
+{
+    return a + b
+}
+// simple and shorter arrow function 
+let sum3 = (a, b) => a + b
+
+// normal function
+function isNegative (negative) 
+{
+    return number <= 0 
+}
+// arrow function
+let isNegative2 = (negative) => number <= 0 
+
+// normal function
+function ranNum ()
+{
+    return Math.random()
+}
+// arrow function
+let ranNum2 = () => Math.random()
