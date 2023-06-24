cd789701e22dd5a7f8a6af05b0187de0157d61fc
Jesus Bejar <israelbejarii@gmail.com>
2023-06-24 16:38:49 -0600

    added url variable w/ test url and headers

---
+++ LyricsProject/scripts/script.js
@@ -0,0 +1,8 @@
+const url = 'https://genius-song-lyrics1.p.rapidapi.com/search/?q=thunderstruck&per_page=10&page=1';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9fa47c65e6msh6013516bc42c8bbp177bd0jsnfe677f674a14',
+		'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+	}
+};
