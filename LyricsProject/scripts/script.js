701c4386b176db0b725916bce3b4f9943980083c
Jesus Bejar <israelbejarii@gmail.com>
2023-07-04 18:38:03 -0600

    made returnSongLyrics() function

--- LyricsProject/scripts/script.js
+++ LyricsProject/scripts/script.js
@@ -14,7 +14,7 @@ let lyricsSection = document.querySelector("lyrics-section");
 
   try {
 	const response = await fetch(url, options);
-	console.log(response);
+	// console.log(response);
 	// .json converts response to json!
 	const result = await response.json();
 	// console.log(result.hits[0].result.id);
@@ -24,3 +24,30 @@ let lyricsSection = document.querySelector("lyrics-section");
   }
 };
 returnSongId();
+
+async function returnSongLyrics(songId) {
+	// 2396871
+	const url = 'https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/?id=291696';
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '9fa47c65e6msh6013516bc42c8bbp177bd0jsnfe677f674a14',
+			'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+		}
+	};
+	
+	try {
+		const response = await fetch(url, options);
+		const result = await response.json();
+		// console.log(result);
+		console.log(result.lyrics.lyrics.body.html);
+		const lyricData = result.lyrics.lyrics.body.html;
+		document.querySelector("#lyrics-section").innerText = `${result.lyrics.lyrics.body.html}`;
+		
+		return lyricData;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+returnSongLyrics();
